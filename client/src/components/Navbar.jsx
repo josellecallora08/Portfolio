facd1068b01f10b3e89b5a0395bfed295cee14d9
Soj <josellecallora08@gmail.com>
2023-12-29 15:23:02 +0800

    fixed resposiveness -mobile

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -26,42 +26,42 @@ const Navbar = () => {
     
   return (
     <div className={`h-20 dark:bg-black`}>
-        <div className="flex items-center justify-between w-11/12 sm:w-98 m-auto h-full">
+        <div className="flex items-center justify-between md:w-11/12 w-96 m-auto h-full">
             <div className="">
                 <h1 className="font-bold dark:text-dark text-light text-2xl sm:text-4xl">SojDev</h1>
             </div>
-            <nav className={`fixed h-full top-20 left-0 text-center w-full sm:m-auto sm:w-auto sm:static duration-150 ${Menu ? `translate-x-full` : `translate-x-none`}`}>
-                <ul className="flex h-full flex-col items-center justify-center gap-5 bg-white sm:bg-transparent sm:flex sm:flex-row sm:gap-20 dark:text-dark text-light font-sans font-bold">
+            <nav className={`fixed h-full w-full top-20 left-0 text-center sm:m-auto sm:w-0 sm:static duration-150 ${Menu ? `translate-x-full` : `translate-x-none`}`}>
+                <ul className="flex h-full flex-col items-center justify-center gap-5 bg-primary dark:bg-secondary sm:bg-transparent sm:flex sm:flex-row sm:gap-20 dark:text-dark text-light font-sans font-bold">
                     <li>
                         <Link>
-                            <span>Home</span>
+                            <span className="text-dark dark:text-light">Home</span>
                         </Link>
                     </li>
                     <li>
                         <Link>
-                            <span>About</span>
+                            <span className="text-dark dark:text-light">About</span>
                         </Link>
                     </li>
                     <li>
                         <Link>
-                            <span>Portfolio</span>
+                            <span className="text-dark dark:text-light">Portfolio</span>
                         </Link>
                     </li>
                     <li>
                         <Link>
-                            <span>Contact</span>
+                            <span className="text-dark dark:text-light">Contact</span>
                         </Link>
                     </li>
                 </ul>
             </nav>
             <div className="flex items-center gap-5 sm:block">
-                <button className={`flex items-center rounded-3xl shrink w-14 h-6 sm:w-16 sm:h-8 bg-white shadow-black shadow-inner`} onClick={handleChange}>
-                    <div className={`w-4 h-4 sm:w-6 sm:h-6 m-2 transition duration-50 ease-in-out rounded-full border-black-80 shadow border  ${isThemeDark ? 'translate-x-6 bg-primary ' : 'bg-yellow-500 '}`}>
+                <button className={`flex items-center rounded-3xl shrink w-16 h-8 bg-white shadow-black shadow-inner`} onClick={handleChange}>
+                    <div className={`w-6 h-6 m-2 transition duration-50 ease-in-out rounded-full border-black-80 shadow border  ${isThemeDark ? 'translate-x-6 bg-primary ' : 'bg-yellow-500 '}`}>
                     </div>
                 </button>
                 <div className={`w-12 h-12 sm:hidden active:rotate-90 duration-100`} onClick={handleNav}>
                     <button>
-                        <img src={Menu ? burger_menu : close_menu} alt="" />
+                        <img src={burger_menu} alt="" />
                     </button>
                 </div>
             </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,30 +26,30 @@ export const Home = () => {
       }, []);
    
     return (
-    <div className={`gap-1 h-screen bg-secondary dark:bg-black  `}>
+    <div className={`gap-1 h-full md:h-screen bg-secondary dark:bg-black  `}>
         <Navbar/>
-        <div className={`w-98 m-auto grid grid-cols-4 h-inherit gap-20`}>
-            <div className={` col-span-1 grid grid-cols-1 grid-rows-4 gap-4`}>
-                <div className="w-full h-full col-span-1 row-span-3 shadow-lg dark:bg-gray-600/30 rounded-md">
+        <div className={`w-full h-auto m-auto grid grid-cols-1 grid-flow-row md:w-98 md:h-inherit md:grid-cols-4 gap-4 md:gap-10`}>
+            <div className={`grid grid-rows-3 m-auto h-full w-full md:w-98 gap-4 row-start-2 md:col-span-1 md:row-start-1 lg:gap-2`}>
+                <div className="w-96 h-full md:row-span-2 mx-auto shadow-lg dark:bg-gray-600/30 rounded-md">
                 2
                </div>
-               <div className="w-full h-full col-span-1 row-span-1 shadow-lg dark:bg-gray-600/30 rounded-md">
-                    <h1 className="text-center font-bold text-2xl p-2 dark:text-dark">LOGIN</h1>
-                    <form className={`grid grid-cols-1 grid-rows-3 gap-2 place-items-center`}>
-                        <div className="w-4/5 h-10 col-span-1 row-span-1 mx-auto shadow-sm rounded-sm overflow-hidden">
+               <div className="hidden md:block row-span-1 shadow-lg dark:bg-gray-600/30 rounded-md">
+                    <h1 className="text-center font-bold text-2xl sm:text-sm p-2 dark:text-dark">LOGIN</h1>
+                    <form className={`grid grid-rows-3 gap-2 place-items-center`}>
+                        <div className="w-4/5 h-10 sm:h-8 row-span-1 mx-auto shadow-sm rounded-sm overflow-hidden">
                             <input type="text" placeholder="Username" className="w-full h-full px-5 outline-none shadow"/>
                         </div>
-                        <div className="w-4/5 h-10 col-span-1 row-span-1 mx-auto shadow-sm rounded-sm overflow-hidden">
+                        <div className="w-4/5 h-10 sm:h-8 row-span-1 mx-auto shadow-sm rounded-sm overflow-hidden">
                             <input type="password" placeholder="Password" name="" id="" className="w-full h-full px-5 outline-none"/>
                         </div>
-                        <button className="w-4/5 h-10 col-span-1 row-span-1 mx-auto shadow-sm rounded-sm overflow-hidden text-white bg-primary font-bold uppercase ease-in-out duration-150 border-2 border-transparent hover:bg-secondary hover:text-light hover:border-primary">
+                        <button className="w-4/5 h-10 sm:h-8 row-span-1 mx-auto shadow-sm rounded-sm overflow-hidden text-white bg-primary font-bold uppercase ease-in-out duration-150 border-1 border-transparent hover:bg-secondary hover:text-light hover:border-primary">
                             Login
                         </button>
                     </form>
                </div>
             </div>
-            <div id="main-container" className="col-span-2 grid gap-4 grid-cols-1 grid-flow-row justify-items-center overflow-y-scroll custom-scrollbar">
-                 {/* <div className="flex flex-col gap-2 justify-around  shadow-lg w-4/5 h-auto py-5 col-span-1 dark:bg-gray-600/30 dark:shadow-sm rounded-lg">
+            <div id="main-container" className="col-span-2 grid gap-4 sm:gap-2 grid-cols-1 grid-flow-row justify-items-center overflow-y-scroll custom-scrollbar">
+                 {/* <div className="w-96 flex flex-col gap-2 justify-around  shadow-lg md:w-4/5 h-auto py-5 col-span-1 dark:bg-gray-600/30 dark:shadow-sm rounded-lg">
                    <button className="w-11/12 m-auto h-10 max-h-max px-5 bg-white rounded-xl outline-none text-left shadow-sm active:bg-gray-300">
                     Create a post...
                     </button>
@@ -73,82 +73,74 @@ export const Home = () => {
                             </label>
                         </div>
                    </div>
-                   <div>
-                   </div>
                 </div> */}
-                <div className="flex justify-around  shadow-lg w-4/5 h-auto py-5 col-span-1 dark:bg-gray-600/30 dark:shadow-sm rounded-lg">
-                    <div className="flex items-center justify-center w-full h-100 overflow-hidden">
-                        <img src={myphoto} alt="" className=" rounded-md object-cover w-4/5 h-full"/>
+                <div className="w-96 grid grid-cols-2 shadow-lg md:w-4/5 md:h-full h-auto py-5 lg:col-span-1 dark:bg-gray-600/30 dark:shadow-sm rounded-lg">
+                    <div className="grid justify-items-center">
+                        <img src={myphoto} alt="" className="rounded-md object-cover w-4/5 h-full"/>
                     </div>
-                    <div className="flex justify-center items-center  w-full h-100">
-                        <div className="text-center font-sans">
-                            <a href="" download><img src={resume} alt="" className="hover:scale-105 ease-in-out duration-150 object-cover rounded-lg w-full h-full" /></a>
-                            <p className="font-bold text-light dark:text-dark mt-3">Joselle E. Callora</p>
-                            <a href="mailto:josellecallora08@gmail.com" className="text:light dark:text-dark hover:underline">josellecallora08@gmail.com</a>
+                    <div className="grid justify-items-center">
+                        <div className="text-center font-sans h-full w-full">
+                            <a href="" download className="grid justify-items-center"><img src={resume} alt="" className="hover:scale-90 ease-in-out duration-150 object-cover rounded-lg w-4/5" /></a>
+                            <p className="text-sm md:text-base font-bold text-light dark:text-dark mt-3">Joselle E. Callora</p>
+                            <a href="mailto:josellecallora08@gmail.com" className="text-sm md:text-base text:light dark:text-dark hover:underline">josellecallora08@gmail.com</a>
                         </div>
                     </div>
                 </div>
-                <div className="h-80 w-4/5 rounded-lg shadow-lg dark:shadow-sm  dark:bg-gray-600/30">
-                    <div className="h-full grid grid-cols-3 gap-4 m-auto w-5/6">
-                        <div className="col-span-1  m-auto">
+                <div className="h-80 w-96 md:w-4/5 rounded-lg shadow-lg dark:shadow-sm  dark:bg-gray-600/30">
+                    <div className="h-full grid md:grid-cols-3 md:gap-4 m-auto w-5/6">
+                        <div className="md:col-span-1 grid justify-items-center m-auto">
                             <h1 className="dark:text-dark font-bold text-lg">Javascript Essentials</h1>
                         </div>
-                        <figure className="col-span-2 bg-white-300 w-full h-4/5 m-auto flex items-center justify-center">
+                        <figure className="md:col-span-2 bg-white-300 w-full h-auto mb-auto grid justify-items-center">
                             <img src={js_cert} alt="Javascript Essentials" className="h-full w-full contain" />
                         </figure>
                     </div>
                 </div>
                 {/* The divs containers will be components */}
-                <div className="h-80 w-4/5 rounded-lg shadow-lg dark:shadow-sm  dark:bg-gray-600/30">
+                <div className="h-70 w-96 md:w-4/5 rounded-lg shadow-lg dark:shadow-sm  dark:bg-gray-600/30">
                     
                 </div>
             </div>
-            <div className={`grid grid-cols-1 grid-rows-5 gap-4 h-full items-start `}>
-               <div className="w-full h-full col-span-1 row-span-2 shadow-lg dark:bg-gray-600/30 rounded-md">
+            <div className={`hidden md:grid grid-rows-5 sm:gap-2 h-full items-start `}>
+               <div className=" w-full h-full row-span-3 shadow-lg dark:bg-gray-600/30 rounded-md">
                     <div className="shadow-md rounded-md overflow-hidden my-5 mx-auto w-4/5 h-10">
                         <input type="search" name="" id="searchbar" placeholder="Search..." className="outline-none px-3 w-full h-full"/>
                     </div>
                     <div className="w-4/5 mx-auto">
-                        <h1 className="flex gap-2 font-sans font-bold uppercase underline dark:text-dark">
+                        <h1 className="flex text-xs sm:text-sm gap-2 font-sans font-bold uppercase underline dark:text-dark">
                             <img src={notif_icon} className="color-red" alt="" />
                             Updates
                         </h1>
                         <div className="flex flex-col gap-2 mt-2">
                             {/* The anchor tanks will be components */}
                             <a href="">
-                                <p className="flex gap-2 ml-5 hover:translate-x-2 dark:text-dark">
+                                <p className="flex text-xs sm:text-sm gap-2 ml-5 hover:translate-x-2 dark:text-dark">
                                     <img src={list_icon} alt="" />
                                     Uploaded MERN Stack Project
                                 </p>
-                            </a>
+                            </a>         
                             <a href="">
-                                <p className="flex gap-2 ml-5 hover:translate-x-2 dark:text-dark">
-                                    <img src={list_icon} alt="" />
-                                    Uploaded MERN Stack Project
-                                    </p>
-                            </a>
-                            <a href="">
-                                <p className="flex gap-2 ml-5 hover:translate-x-2 dark:text-dark">
+                                <p className="flex text-xs sm:text-sm gap-2 ml-5 hover:translate-x-2 dark:text-dark">
                                     <img src={list_icon} alt="" />
                                     Uploaded MERN Stack Project
                                 </p>
-                            </a>
+                            </a>   
                             <a href="">
-                                <p className="flex gap-2 ml-5 hover:translate-x-2 dark:text-dark">
+                                <p className="flex text-xs sm:text-sm gap-2 ml-5 hover:translate-x-2 dark:text-dark">
                                     <img src={list_icon} alt="" />
                                     Uploaded MERN Stack Project
                                 </p>
-                            </a>
+                            </a>  
                             <a href="">
-                                <p className="flex gap-2 ml-5 hover:translate-x-2 dark:text-dark">
+                                <p className="flex text-xs sm:text-sm gap-2 ml-5 hover:translate-x-2 dark:text-dark">
                                     <img src={list_icon} alt="" />
                                     Uploaded MERN Stack Project
                                 </p>
-                            </a>
+                            </a>                 
                         </div>
                     </div>
                 </div>
-                <div className="w-full h-full col-span-1 row-span-3 shadow-lg dark:bg-gray-600/30 rounded-md">
+                <div className="w-full h-full row-span-2 shadow-lg dark:bg-gray-600/30 rounded-md">
                 2
                </div>
             </div>
